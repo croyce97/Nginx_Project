e1f453f7155d6a75124169a3376df61beb534957
Nguyễn Quang Cảnh <125856531+croyce97@users.noreply.github.com>
2025-07-24 13:41:07 +0700

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,8 +1,8 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-  user: "canhnq",
-  password: "canhnq",
+  user: "postgres",
+  password: "postgres",
   host: "localhost",
   port: 5432,
   database: "perntodo"
